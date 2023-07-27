6555a46044655679c5538f450e7d87f483fed6ad
Lucas Balieiro <lucas.matos@indt.org.br>
2023-07-26 23:02:16 -0400

    feat: add web analytics for vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <Analytics />
       </body>
     </html>
   )
